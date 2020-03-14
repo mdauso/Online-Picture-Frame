3fe3b21f3f4714d377b41b879be0dfe601729f6e
Mario Deuse <62134772+mdauso@users.noreply.github.com>
2020-03-14 16:39:27 +0100

    Delete UploadLog.sh

--- bin/UploadLog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# change to directory of this script
-cd "$(dirname "$0")"
-
-	### Send Log File to FTP Folder
-	/mnt/us/usbnet/bin/curl -T /mnt/us/extensions/onlinepictureframe/bin/pictureframe.log ftp://mdauso:Hecke2009@mario-deuse.my-router.de:21/Logfiles/ 
-
